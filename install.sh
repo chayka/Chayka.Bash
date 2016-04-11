7d4059005ab36f84e388a2c56c37116f493223f7
Boris Mossounov <borix@tut.by>
2016-04-11 17:28:30 +0300

    Minor update Install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@
 #
 
 chmod u+x chayka.sh
+
 cwd=$(pwd)
 ln -s ${cwd}/chayka.sh /usr/local/bin/chayka
 ln -s ${cwd}/etc/nginx/chayka/ /etc/nginx/chayka
+
 if [ ! -z $1 ]; then
 export MYSQL_ROOT_PASSWORD=$1
 echo "export MYSQL_ROOT_PASSWORD=$1" >> ~/.bashrc
 fi
+
 echo "Chayka bash helper script is good to go now!"

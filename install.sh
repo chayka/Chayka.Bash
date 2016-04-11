409c479c4bdff284555756132ec91f5ff550646f
Boris Mossounov <borix@tut.by>
2016-04-11 17:26:08 +0300

    Initial commit

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@
 #
 
 chmod u+x chayka.sh
-ln -s ./chayka.sh /usr/local/bin/chayka
-ln -s ./etc/nginx/chayka/ /etc/nginx/chayka
+cwd=$(pwd)
+ln -s ${cwd}/chayka.sh /usr/local/bin/chayka
+ln -s ${cwd}/etc/nginx/chayka/ /etc/nginx/chayka
 if [ ! -z $1 ]; then
 export MYSQL_ROOT_PASSWORD=$1
 echo "export MYSQL_ROOT_PASSWORD=$1" >> ~/.bashrc
